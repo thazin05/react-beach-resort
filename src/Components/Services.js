f2aa2ffbaa6ee10414cf033f0a656c0176daf862
Zaw Win Htet <zawwinhtet@Zaws-MacBook-Pro.local>
2021-05-19 10:50:49 +0800

    update services

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,29 +1,39 @@
 import React, { Component } from 'react';
-import {FaCocktail,FaHiking,FaShuttleVan,FaBeer} from 'react-icons/fa';
+import {FaCocktail,FaHiking,FaCanadianMapleLeaf,FaMapSigns,FaSkiingNordic,FaKiwiBird,FaCampground} from 'react-icons/fa';
 import Title from './Title';
 
 export default class Services extends Component {
   state={
       services: [
           {
-              icon:<FaCocktail/>,
-              title:'Free Cocktails',
-              info:'Lorem ipsum dolor sit amet cnsectetur adipisicing elit. Magni, corpiors!'
+              icon:<FaSkiingNordic/>,
+              title:'Wineries Tour',
+              info:'he Shan Region is home of two stunning Vineyard, the Red Mountain Estate at Nyaung Shwe  and the  Aythaya Estate in Taunggyi'
           },
           {
+            icon:<FaCanadianMapleLeaf/>,
+            title:'Cultural Sites',
+            info:"Kalaw region is rich on cultural Buddhist temple and pagodas, close to Pindaya's world famous limestone caves with its thousand  of Buddha Images."
+        },
+        {
+            icon:<FaMapSigns/>,
+            title:'Market Tour',
+            info:'Kalaw’s heart is its market, where villagers from the surrounding hills come to sell their products such as vegetables, handcraft, herbs & spices from the region.'
+        },
+        {
             icon:<FaHiking/>,
-            title:'Endless Hiking',
-            info:'Lorem ipsum dolor sit amet cnsectetur adipisicing elit. Magni, corpiors!'
+            title:'Leisure Activities',
+            info:'Kalaw is the ideal starting base for hiking, biking or bird watching trips. The trek to Inle Lake is a must for every adventurer.'
         },
         {
-            icon:<FaShuttleVan/>,
-            title:'Free Shuttle',
-            info:'Lorem ipsum dolor sit amet cnsectetur adipisicing elit. Magni, corpiors!'
+          icon:<FaKiwiBird/>,
+          title:'Birds Safari',
+          info:'Specialist tour guides and  ornithologists will introduce you to the world of the evergreen forest wildlife  and its hundreds recorded bird species.'
         },
         {
-            icon:<FaBeer/>,
-            title:'Strongest Beer',
-            info:'Lorem ipsum dolor sit amet cnsectetur adipisicing elit. Magni, corpiors!'
+          icon:<FaCampground/>,
+          title:'Green Hill Valley Elephant Camp',
+          info:'A wonderful facility founded in 2011, with the finest nature reserves for retired working elephants, feed, bathe and interact with the elephants throughout the day.'
         }
       ]
   }
