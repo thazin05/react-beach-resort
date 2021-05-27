6935c1b2749809d217f5b7636556f4c142435438
Zaw Win Htet <zawwinhtet@Zaws-MacBook-Pro.local>
2021-05-28 00:33:13 +0800

    updated map

--- src/Components/map.js
+++ src/Components/map.js
@@ -3,7 +3,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 export default function map() {
   return (
-      <MapContainer center={[20.634890, 96.565460]} zoom={10} scrollWheelZoom={true}>
+      <MapContainer center={[20.634890, 96.565460]} zoom={8} scrollWheelZoom={true}>
           <TileLayer
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
